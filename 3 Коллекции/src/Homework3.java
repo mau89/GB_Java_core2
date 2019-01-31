f9156253f2dd00425832035d6595f52aeffbb8d6
mau89 <43510463+mau89@users.noreply.github.com>
2019-01-31 20:41:53 +0500

    Исправил по комментарию в телеграмме
    поиск реализовал через containsKey

--- 3 Коллекции/src/Homework3.java
+++ 3 Коллекции/src/Homework3.java
@@ -4,11 +4,11 @@ public class Homework3 {
         task1.task1();
 
         Tel tel = new Tel();
-        tel.add("123", "Иванов");
-        tel.add("456", "Петров");
-        tel.add("789", "Сидоров");
-        tel.add("987", "Иванов");
-        tel.add("477", "Сидоров");
+        tel.add("Иванов","555555");
+        tel.add("Петров","456");
+        tel.add("Сидоров","789");
+        tel.add("Иванов","987");
+        tel.add("Сидоров","477");
 
         tel.get("Иванов");
     }
--- 3 Коллекции/src/Tel.java
+++ 3 Коллекции/src/Tel.java
@@ -1,17 +1,21 @@
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 
 public class Tel {
     private HashMap<String, String> hashMap = new HashMap<>();
 
-    public void get(String value) {
-        for (HashMap.Entry<String, String> hasp : this.hashMap.entrySet()) {
-            if (hasp.getValue() == value) {
-                System.out.printf("Телефон- %s, Фамилия - %s%n", hasp.getKey(), hasp.getValue());
-            }
-        }
+    public void get(String key) {
+        if (hashMap.containsKey(key)){
+            System.out.printf("Телефон- %s, Фамилия - %s%n", key, hashMap.get(key));
+
+        } else System.out.println("Не найдено такой фамилии");
     }
 
     public void add(String key, String value) {
-        this.hashMap.put(key, value);
+        if (hashMap.containsKey(key)){
+            hashMap.put(key,hashMap.get(key)+", "+value);
+        } else this.hashMap.put(key, value);
     }
 }
