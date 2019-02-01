d075ae04391b8ef990f47ceec72e6958eb46e0c9
mau89 <43510463+mau89@users.noreply.github.com>
2019-02-01 18:59:00 +0500

    Переделал дз после проверка

--- 3 Коллекции/src/Task1.java
+++ 3 Коллекции/src/Task1.java
@@ -2,28 +2,30 @@ import java.util.*;
 
 public class Task1 {
     public static void task1() {
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add("мама");
-        strings.add("мыла");
-        strings.add("раму");
-        strings.add("раму");
-        strings.add("мыла");
-        strings.add("с");
-        strings.add("мылом");
-        strings.add("разик");
-        strings.add("промахнулась");
-        strings.add("и");
-        strings.add("попала");
-        strings.add("мимо");
-        for (int i = 0; i < strings.size(); i++) {
-            if (Collections.frequency(strings, strings.get(i)) == 1) {
-                System.out.println(strings.get(i));
-            }
+        ArrayList<String> list = new ArrayList<>();
+        list.add("мама");
+        list.add("мыла");
+        list.add("раму");
+        list.add("раму");
+        list.add("мыла");
+        list.add("с");
+        list.add("мылом");
+        list.add("разик");
+        list.add("промахнулась");
+        list.add("и");
+        list.add("попала");
+        list.add("мимо");
+        Map<String,Integer> map = new HashMap<>();
+        for (int i = 0; i < list.size(); i++) {
+           if (!map.containsKey(list.get(i))){
+               map.put(list.get(i),1);
+           }else {
+               map.put(list.get(i),map.get(list.get(i))+1);
+           }
         }
-        Set<String> set = new LinkedHashSet<>(strings);
         System.out.printf("%n");
-        for (String linkedHashMap : set) {
-            System.out.printf("Cлово \"%s\" количество повторений - %d; %n", linkedHashMap, Collections.frequency(strings, linkedHashMap));
+        for (Map.Entry<String,Integer> finalmap : map.entrySet()) {
+            System.out.printf("Cлово \"%s\" количество повторений - %d; %n", finalmap.getKey(), finalmap.getValue());
         }
     }
 }
--- 3 Коллекции/src/Tel.java
+++ 3 Коллекции/src/Tel.java
@@ -1,21 +1,22 @@
-import javax.sound.midi.Soundbank;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class Tel {
-    private HashMap<String, String> hashMap = new HashMap<>();
+    private Map<String, List<String>> hashMap = new HashMap<>();
 
     public void get(String key) {
         if (hashMap.containsKey(key)){
-            System.out.printf("Телефон- %s, Фамилия - %s%n", key, hashMap.get(key));
+            System.out.printf("Фамилия- %s, телефон - %s%n", key, hashMap.get(key));
 
         } else System.out.println("Не найдено такой фамилии");
     }
 
     public void add(String key, String value) {
         if (hashMap.containsKey(key)){
-            hashMap.put(key,hashMap.get(key)+", "+value);
-        } else this.hashMap.put(key, value);
+            hashMap.get(key).add(value);
+        } else {
+            List<String> aaa=new ArrayList();
+            aaa.add(value);
+            this.hashMap.put(key, aaa);
+        }
     }
 }
