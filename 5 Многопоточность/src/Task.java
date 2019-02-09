06829d76bef727ab237d81b536754b58afc1c9a1
mau89 <43510463+mau89@users.noreply.github.com>
2019-02-09 21:34:49 +0500

    Поправил замечания

--- 5 Многопоточность/src/Task.java
+++ 5 Многопоточность/src/Task.java
@@ -1,6 +1,7 @@
+import java.util.Arrays;
 public class Task {
-    static final int size = 10000000;
-    static final int h = size / 2;
+    static final int SIZE = 10_000_000;
+    static final int H = SIZE / 2;
 
     public static void main(String[] args) throws InterruptedException {
         Version1();
@@ -8,10 +9,8 @@ public class Task {
     }
 
     public static void Version1() {
-        float[] arr = new float[size];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1;
-        }
+        float[] arr = new float[SIZE];
+        Arrays.fill(arr, 1);
 
         long a = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
@@ -21,36 +20,31 @@ public class Task {
     }
 
     public static void Version2() throws InterruptedException {
-        float[] arr = new float[size];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1;
-        }
+        float[] arr = new float[SIZE];
+        Arrays.fill(arr, 1);
         long a = System.currentTimeMillis();
-        float[] a1 = new float[h];
-        float[] a2 = new float[h];
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
+        float[] a1 = new float[H];
+        float[] a2 = new float[H];
+        System.arraycopy(arr, 0, a1, 0, H);
+        System.arraycopy(arr, H, a2, 0, H);
 
-        Runnable runnable1 = new Runnable() {
+        class MyRunnable implements Runnable {
+            float[] arr1;
+
+            public MyRunnable(float[] arr1) {
+                this.arr1 = arr1;
+            }
+
             @Override
             public void run() {
-                for (int i = 0; i < a1.length; i++) {
-                    a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                for (int i = 0; i < arr1.length; i++) {
+                    arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
             }
-        };
+        }
 
-        Runnable runnable2 = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < a2.length; i++) {
-                    a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
-            }
-        };
-
-        Thread thread1 = new Thread(runnable1);
-        Thread thread2 = new Thread(runnable2);
+        Thread thread1 = new Thread(new MyRunnable(a1));
+        Thread thread2 = new Thread(new MyRunnable(a2));
 
         thread1.start();
         thread2.start();
@@ -58,8 +52,8 @@ public class Task {
         thread1.join();
         thread2.join();
 
-        System.arraycopy(a1, 0, arr, 0, h);
-        System.arraycopy(a2, 0, arr, h, h);
+        System.arraycopy(a1, 0, arr, 0, H);
+        System.arraycopy(a2, 0, arr, H, H);
 
         System.out.printf("Вариант выполнения двух потоков %s секунд %n", (float) (System.currentTimeMillis() - a) / 1000);
     }
